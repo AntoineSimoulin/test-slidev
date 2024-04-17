c7a87df40ad5b123c140955340fdb4dce8cdc0b4
AntoineSimoulin <antoine.simoulin@gmail.com>
2024-04-16 19:23:39 -0500

    Updates

--- assets/channel-d3ac35ac-BYK4EQsR.js
+++
@@ -1,1 +0,0 @@
-import{aB as o,aC as r}from"./slidev/12-DBtJ4U82.js";const s=(a,n)=>o.lang.round(r.parse(a)[n]),e=s;export{e as c};
---
+++ presentation1/assets/channel-d3ac35ac-Bshl_Nvx.js
@@ -0,0 +1,1 @@
+import{aB as o,aC as r}from"./slidev/12-RB9ZF9qZ.js";const s=(a,n)=>o.lang.round(r.parse(a)[n]),e=s;export{e as c};
